f54895c4a249c6bbbdc6f217b2531c124c82d01e
Elton Andrew <elton.andrew@airpay.co.in>
2020-04-13 16:35:46 +0530

    Search, Users component made

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './component/layouts/Navbar';
 import Search from './component/layouts/Search';
+import Users from './component/users/Users';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    value : '',
+    users:[],
   }
+
+  changeState = (users) =>{
+    if(users === "")
+      axios.get('https://api.github.com/users').then(result=>this.setState({users:result.data}));
+    else
+      axios.get('https://api.github.com/users/'+users).then(result=>this.setState({users:[result.data]}));
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users').then(result=>this.setState({users:result.data}));
+  }
+
   render(){
     return (
       <div className="App">
         <Navbar title="GitHub Explorer" icon="fa fa-github">
-          <Search placeholder="Search for Users, Projects.." buttonIcon = "fa fa-search" value={this.state.value}/>
+          <Search changeState={this.changeState} placeholder="Search for Users, Projects.." buttonIcon = "fa fa-search" value={this.state.value}/>
         </Navbar>
+        <Users users={this.state.users}/>
       </div>
     );
   }
--- src/component/layouts/Search.js
+++ src/component/layouts/Search.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import PropTypes from 'prop-types'
 
 export class Search extends Component {
+
+    constructor(props){
+        super(props);
+        this.searchInput = createRef();
+    }
+
     static propTypes = {
         placeholder : PropTypes.string.isRequired,
         buttonIcon : PropTypes.string.isRequired,
         value : PropTypes.string.isRequired,
     }
 
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        this.props.changeState(this.searchInput.current.value);
+    }
+
     render() {
         return (
             <div className ="Search">
-                <input type="text" placeholder={this.props.placeholder} defaultValue={this.props.value}></input>
-                <button className={this.props.buttonIcon}></button>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder={this.props.placeholder} defaultValue={this.props.value} ref={this.searchInput}></input>
+                    <button className={this.props.buttonIcon}></button>
+                </form>
             </div>
         )
     }
